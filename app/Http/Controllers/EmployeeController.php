577a27f73d3cb92257c1d787bff1ebbe514d305b
Ralfhstr <69681774+Ralfhstr@users.noreply.github.com>
2023-05-28 19:47:02 +0700

    Add files via upload

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Employee;
+use App\Models\Position;
+use Illuminate\Http\Request;
+use Validator;
+use Illuminate\Support\Facades\DB;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $pageTitle = 'Employee List';
+
+        // memanggil semua data pada employee
+        $employees = Employee::all();
+
+        return view('employee.index', [
+            'pageTitle' => $pageTitle,
+            'employees' => $employees
+        ]);
+
+        // // RAW SQL QUERY
+        // $employees = DB::select('
+        //     select *, employees.id as employee_id, positions.name as position_name from employees
+        //     left join positions on employees.position_id = positions.id
+        // ');
+
+        // // Query Builder
+        // $employees = DB::table('employees')
+        // ->select('*', 'employees.id as employee_id', 'positions.name as position_name')
+        // ->leftJoin('positions','employees.position_id', '=', 'positions.id')
+        // ->get();
+
+
+    return view('employee.index', [
+        'pageTitle' => $pageTitle,
+        'employees' => $employees
+    ]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $pageTitle = 'Create Employee';
+
+        // ELOQUENT
+        $positions = Position::all();
+
+        // // RAW SQL Query
+        // $positions = DB::select('select * from positions');
+
+        // // Query Builder
+        // $positions = DB::table('positions')->get();
+
+
+        return view('employee.create', compact('pageTitle', 'positions'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $messages = [
+            'required' => ':Attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar',
+            'numeric' => 'Isi :attribute dengan angka'
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // menambahkan data employee sesuai input
+        $employee = New Employee;
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->email = $request->email;
+        $employee->age = $request->age;
+        $employee->position_id = $request->position;
+        $employee->save();
+
+
+        // // INSERT QUERY
+        // DB::table('employees')->insert([
+        //     'firstname' => $request->firstName,
+        //     'lastname' => $request->lastName,
+        //     'email' => $request->email,
+        //     'age' => $request->age,
+        //     'position_id' => $request->position,
+        // ]);
+
+        return redirect()->route('employees.index');
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $pageTitle = 'Employee Detail';
+
+        // menampilkan data sesuai dengan id yang dipilih
+        $employee = Employee::find($id);
+
+        // // RAW SQL QUERY
+        // $employee = collect(DB::select('
+        //     select *, employees.id as employee_id, positions.name as position_name
+        //     from employees
+        //     left join positions on employees.position_id = positions.id
+        //     where employees.id = ?
+        // ', [$id]))->first();
+
+        // // Query Builder
+        // $employee = DB::table('employees')
+        //             ->select('*', 'employees.id as employee_id', 'positions.name as position_name')
+        //             ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
+        //             ->where('employees.id', '=', $id)
+        //             ->first();
+
+        return view('employee.show', compact('pageTitle', 'employee'));
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $pageTitle = 'Employee Edit';
+
+        // menampilkan semua data pada tabel position
+        $positions = Position::all();
+        // menampilkan data sesuai dengan id yang dipilih
+        $employee = Employee::find($id);
+
+        return view('employee.edit', compact('pageTitle', 'positions', 'employee'));
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $messages = [
+            'required' => ':Attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar',
+            'numeric' => 'Isi :attribute dengan angka'
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // update data sesuai id yang dipilih
+        $employee = Employee::find($id);
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->email = $request->email;
+        $employee->age = $request->age;
+        $employee->position_id = $request->position;
+        $employee->save();
+
+
+        // DB::table('employees')
+        // ->where('id', $id)
+        // ->update([
+        //     'firstname' => $request->input('firstName'),
+        //     'lastname' => $request->input('lastName'),
+        //     'email' => $request->input('email'),
+        //     'age' => $request->input('age'),
+        //     'position_id' => $request->input('position')
+        // ]);
+
+        return redirect()->route('employees.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        // menghapus data sesuai id yang dipilih 
+        Employee::find($id)->delete();
+
+        return redirect()->route('employees.index');
+    }
+}
---
+++ resources/views/default.blade.php
@@ -0,0 +1,9 @@
+<div class="container mt-4">
+    {{-- menampikan data yang dipassing pada controller --}}
+    <h4>{{ $pageTitle }}</h4>
+    <hr>
+    <div class="d-flex align-items-center py-2 px-4 bg-light rounded-3 border">
+        <div class="bi-house-fill me-3 fs-1"></div>
+        <h4 class="mb-0">Well done! this is {{ $pageTitle }}.</h4>
+    </div>
+</div>
